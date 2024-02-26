ab05fe11433c4b318db662b4047a6bc3d4addfdc
Sebastian Gonzales-Portillo <sebasagpf@gmail.com>
2024-02-26 14:32:10 -0500

    fixed bug in task script

--- task.sh
+++ task.sh
@@ -5,28 +5,28 @@ echo "Executing the training scripts"
 # this script will computed adjusted cosine similarity and L2 distance of feature vectors
 
 #load resnet18 mnist, compute adj_cos sim and L2 on mnist
-python Scripts/script.py --execution_id "2024-02-26-08-25-11" --cuda --model resnet18 --train_dataset mnist  --adj_cos_sim --l2 --desc "load mnist resnet18, compute adj_cos_sim and L2 on mnist"
+python Scripts/script.py --execution_id "2024-02-26-08-25-11" --cuda --model resnet18 --train_dataset mnist  --cos_sim_adj --l2 --desc "load mnist resnet18, compute cos_sim_adj and L2 on mnist"
 
 #load resnet18 mnist, compute adj_cos sim and L2 on fashion_mnist
-python Scripts/script.py --execution_id "2024-02-26-08-25-11" --cuda --model resnet18 --train_dataset mnist  --adj_cos_sim --l2 --cs_dataset fashion_mnist --desc "load mnist resnet18, compute adj_cos_sim and L2 on fashion_mnist"
+python Scripts/script.py --execution_id "2024-02-26-08-25-11" --cuda --model resnet18 --train_dataset mnist  --cos_sim_adj --l2 --cs_dataset fashion_mnist --desc "load mnist resnet18, compute cos_sim_adj and L2 on fashion_mnist"
 
 #load resnet18 fashion_mnist, compute adj_cos sim and L2 on fashion_mnist
-python Scripts/script.py --execution_id "2024-02-26-08-33-57" --cuda --model resnet18 --train_dataset fashion_mnist  --adj_cos_sim --l2 --desc "load fashion_mnist resnet18, compute adj_cos_sim and L2 on fashion_mnist"
+python Scripts/script.py --execution_id "2024-02-26-08-33-57" --cuda --model resnet18 --train_dataset fashion_mnist  --cos_sim_adj --l2 --desc "load fashion_mnist resnet18, compute cos_sim_adj and L2 on fashion_mnist"
 
 #load resnet18 fashion_mnist, compute adj_cos sim and L2 on mnist
-python Scripts/script.py --execution_id "2024-02-26-08-33-57" --cuda --model resnet18 --train_dataset fashion_mnist  --adj_cos_sim --l2 --cs_dataset mnist --desc "load fashion_mnist resnet18, compute adj_cos_sim and L2 on mnist"
+python Scripts/script.py --execution_id "2024-02-26-08-33-57" --cuda --model resnet18 --train_dataset fashion_mnist  --cos_sim_adj --l2 --cs_dataset mnist --desc "load fashion_mnist resnet18, compute cos_sim_adj and L2 on mnist"
 
 #load resnet18 cifar10, compute adj_cos sim and L2 on cifar10
-python Scripts/script.py --execution_id "2024-02-26-10-35-43" --cuda --model resnet18 --train_dataset cifar10  --adj_cos_sim --l2 --desc "load cifar10 resnet18, compute adj_cos_sim and L2 on cifar10"
+python Scripts/script.py --execution_id "2024-02-26-10-35-43" --cuda --model resnet18 --train_dataset cifar10  --cos_sim_adj --l2 --desc "load cifar10 resnet18, compute cos_sim_adj and L2 on cifar10"
 
 #load resnet18 cifar10, compute adj_cos sim and L2 on cifar100
-python Scripts/script.py --execution_id "2024-02-26-10-35-43" --cuda --model resnet18 --train_dataset cifar10  --adj_cos_sim --l2 --cs_dataset cifar100 --desc "load cifar10 resnet18, compute adj_cos_sim and L2 on cifar100"
+python Scripts/script.py --execution_id "2024-02-26-10-35-43" --cuda --model resnet18 --train_dataset cifar10  --cos_sim_adj --l2 --cs_dataset cifar100 --desc "load cifar10 resnet18, compute cos_sim_adj and L2 on cifar100"
 
 #load resnet18 cifar100, compute adj_cos sim and L2 on cifar100
-python Scripts/script.py --execution_id "2024-02-26-09-03-16" --cuda --model resnet18 --train_dataset cifar100  --adj_cos_sim --l2 --desc "load cifar100 resnet18, compute adj_cos_sim and L2 on cifar100"
+python Scripts/script.py --execution_id "2024-02-26-09-03-16" --cuda --model resnet18 --train_dataset cifar100  --cos_sim_adj --l2 --desc "load cifar100 resnet18, compute cos_sim_adj and L2 on cifar100"
 
 #load resnet18 cifar100, compute adj_cos sim and L2 on cifar10
-python Scripts/script.py --execution_id "2024-02-26-09-03-16" --cuda --model resnet18 --train_dataset cifar100  --adj_cos_sim --l2 --cs_dataset cifar10 --desc "load cifar100 resnet18, compute adj_cos_sim and L2 on cifar10"
+python Scripts/script.py --execution_id "2024-02-26-09-03-16" --cuda --model resnet18 --train_dataset cifar100  --cos_sim_adj --l2 --cs_dataset cifar10 --desc "load cifar100 resnet18, compute cos_sim_adj and L2 on cifar10"
 
 
 echo "All the scripts have been executed"
